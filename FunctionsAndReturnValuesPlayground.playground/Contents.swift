6581d1d5f9010af2d27dffe6bdc4817b6a9b006b
MadinaIbrahim <madina.ibrahim.us@gmail.com>
2016-07-06 09:23:05 -0400

    Done.

---
+++ FunctionsAndReturnValuesPlayground.playground/Contents.swift
@@ -0,0 +1,36 @@
+func printAge(name: String) {
+    print("\(name) is 29")
+}
+printAge("Bettina")
+
+func happyBirthday(age: Int) {
+    print("Happy birthday #\(age)!")
+}
+
+func getAge(name: String) -> Int {
+    return 29
+}
+
+let friend = "Emily"
+var friendAge = getAge(friend)
+happyBirthday(friendAge)
+
+func birthdayGreeting(age: Int) -> String {
+    return "Happy birthday #\(age)!"
+}
+
+birthdayGreeting(30)
+
+var greeting = birthdayGreeting(30)
+print(greeting)
+
+func getAgeAndCongratulate(name: String) -> Int {
+    let age = 29
+    print("Happy \(age)th birthday, \(name)!")
+    return age
+}
+
+let friend2 = "Pete"
+var friend2Age = getAgeAndCongratulate(friend2)
+
+
